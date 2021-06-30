f04a0608737414dbf876dd39c02e715421a69c33
devfeefung79 <70851914+devfeefung79@users.noreply.github.com>
2021-07-01 02:26:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,22 +9,27 @@ function compute() {
   yr = document.getElementById("years").value;
 
   if (p == null || p == "") {
-    p = 0;
-  }
-  let totalAmount = ((p * rate) / 100) * parseInt(yr);
-  let currYr = new Date().getFullYear();
-  let endYr = currYr + parseInt(yr);
-  let displayText =
-    'If you deposit <span class="hightlight-text">' +
-    p +
-    "</span>,<br/>" +
-    'at an interest rate of <span class="hightlight-text">' +
-    rate +
-    '%</span>.<br/> You will receive an amount of <span class="hightlight-text">' +
-    totalAmount +
-    '</span>,<br/> in the year <span class="hightlight-text">' +
-    endYr +
-    "</span>";
+    alert("Please enter a positive number for amount!");
+    document.getElementById("principal").focus();
+  } else if (p <= 0) {
+    alert("Please enter a positive number for amount!");
+    document.getElementById("principal").focus();
+  } else {
+    let totalAmount = ((p * rate) / 100) * parseInt(yr);
+    let currYr = new Date().getFullYear();
+    let endYr = currYr + parseInt(yr);
+    let displayText =
+      'If you deposit <span class="hightlight-text">' +
+      p +
+      "</span>,<br/>" +
+      'at an interest rate of <span class="hightlight-text">' +
+      rate +
+      '%</span>.<br/> You will receive an amount of <span class="hightlight-text">' +
+      totalAmount +
+      '</span>,<br/> in the year <span class="hightlight-text">' +
+      endYr +
+      "</span>";
 
-  document.getElementById("result").innerHTML = displayText;
+    document.getElementById("result").innerHTML = displayText;
+  }
 }
